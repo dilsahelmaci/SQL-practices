f3fe82b410a06b47e9453b1c362bd6d5e82d430f
Dilsah <92085453+dilsahelmaci@users.noreply.github.com>
2025-03-27 21:07:58 +0100

    Add files via upload

---
+++ 03-TikTok_SecondDayConfirmation/solution.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT user_id
+FROM emails
+INNER JOIN texts
+  ON emails.email_id = texts.email_id
+WHERE signup_action = 'Confirmed'
+  AND (action_date::DATE) - (signup_date::DATE) = 1;
